47a8867b3982c18f8b426be7f1f382afc04aabe8
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-15 08:49:08 +0530

    Validating user input

--- Frog_assigment/src/Frog.java
+++ Frog_assigment/src/Frog.java
@@ -1,2 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Frog {
+
+     void Frog(){
+         Scanner scanner = new Scanner(System.in);
+         boolean validate = false;
+         int distance = 0;
+         int jump1 = 0;
+         int jump2 = 0;
+         int jump3 = 0;
+
+         do {
+             System.out.println("Enter the distance");
+             try {
+                 distance = scanner.nextInt();
+                 validate = true;
+
+             }catch (InputMismatchException e){
+
+                 System.out.println("Input has to be a number");
+             }
+
+         }while (validate==false);
+
+
+
+     }
 }
