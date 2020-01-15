23a50a118a8699c97acdb91c627b0cd602c3cf7a
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-15 11:55:19 +0530

    Created distance calculation method

--- Frog_assigment/src/Frog.java
+++ Frog_assigment/src/Frog.java
@@ -2,29 +2,47 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Frog {
+    public static void main(String[] args) {
+        Frog a = new Frog();
+        a.FrogCalculateDistance();
+    }
 
-     void Frog(){
-         Scanner scanner = new Scanner(System.in);
-         boolean validate = false;
-         int distance = 0;
-         int jump1 = 0;
-         int jump2 = 0;
-         int jump3 = 0;
+    void FrogCalculateDistance() {
 
-         do {
-             System.out.println("Enter the distance");
-             try {
-                 distance = scanner.nextInt();
-                 validate = true;
-
-             }catch (InputMismatchException e){
-
-                 System.out.println("Input has to be a number");
-             }
-
-         }while (validate==false);
+        Scanner scanner = new Scanner(System.in);
+        boolean validate = false;
+        int time = 0;
 
 
+        do {
+            System.out.println("Enter the Time");
+            try {
+                time = scanner.nextInt();
+                validate = true;
 
-     }
+            } catch (InputMismatchException e) {
+
+                System.out.println("Input has to be a number");
+                break;
+            }
+
+        } while (validate == false);
+
+           int remainder = time%8;
+
+         if (remainder==0){
+
+             System.out.println("Distance traveled by jhon is "+((time/8)*9)+" m ");
+         }
+         else if(remainder<=1){
+             System.out.println("Distance traveled by jhon is " +((time/8)*9+5)+ " m ");
+
+         }else if(remainder<=3) {
+             System.out.println("Distance traveled by jhon is " + ((time / 8) * 9 + 8) + " m ");
+         }
+         else if(remainder<=7) {
+             System.out.println("Distance traveled by jhon is " + ((time / 8) * 9 + 9) + " m ");
+         }
+
+    }
 }
