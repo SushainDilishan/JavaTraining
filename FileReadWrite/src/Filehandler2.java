216238b9b647d2f68fceed03ecc7a2bbe7315cf3
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-19 20:21:29 +0530

    jar file created

--- FileReadWrite/src/Filehandler2.java
+++ FileReadWrite/src/Filehandler2.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class Filehandler2 {
+    public static void main(String[] args) {
+        String Path = "D:/aa.txt";
+        try(InputStream inputStream = new FileInputStream(Path)) {
+            String fileContent = "This is a sample text.";
+            inputStream.(fileContent);
+        } catch (IOException e) {
+
+        }
+        try(FileReader fileReader = new FileReader(Path)) {
+            int ch = fileReader.read();
+            while(ch != -1) {
+                System.out.print((char)ch);
+                ch = fileReader.read();
+            }
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+
+        }
+    }
 }
