c2dda4113cd97bce6a15826d362d711db2523365
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-21 00:02:04 +0530

    Updated Names

--- HashMapAssigment/src/Application.java
+++ HashMapAssigment/src/Application.java
@@ -5,11 +5,11 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         HashMap<String,Integer> hashMap = new HashMap<>();
-        hashMap.put("Krish",80);
-        hashMap.put("iresh",70);
-        hashMap.put("Dinuka",60);
-        hashMap.put("dhanuka",90);
-        hashMap.put("Sushain",70);
+        hashMap.put("aaa",80);
+        hashMap.put("bbb",70);
+        hashMap.put("ccc",60);
+        hashMap.put("ddd",90);
+        hashMap.put("eee",70);
 
         List<String> employees = hashMap.keySet().stream().filter((s) -> hashMap.get(s) > 60).sorted().collect(Collectors.toList());
         System.out.println(employees);
