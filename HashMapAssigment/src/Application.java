1f555dc71ab0aad04fdb0f200db78ee8f21224bb
Sushain Dilishan <Sushaind96@gmail.com>
2020-01-20 23:34:12 +0530

    updated the code

---
+++ HashMapAssigment/src/Application.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Application {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        hashMap.put("Krish",80);
+        hashMap.put("iresh",70);
+        hashMap.put("Dinuka",60);
+        hashMap.put("Krish",50);
+        hashMap.put("Sushain",60);
+        
+    }
+}
